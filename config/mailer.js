bcaef564e9e7e57f3484f81d6556c1adb1d24e50
mohibr33 <riazmohib@gmail.com>
2025-11-01 15:32:25 +0500

    Change mailer.js file

--- config/mailer.js
+++ config/mailer.js
@@ -3,15 +3,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.EMAIL_HOST,   // mail.gmx.com
+  port: process.env.SMTP_PORT,    // 587
+  secure: false,                  // false for port 587 (STARTTLS)
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: process.env.EMAIL_USER, // SEOINTRUSION-DETECTOR@gmx.com
+    pass: process.env.EMAIL_PASS  // mohib@3764
+  },
+  tls: {
+    rejectUnauthorized: false     // optional: allows self-signed certificates
   }
 });
 
 // Test email connection on startup
-transporter.verify(function (error, success) {
+transporter.verify((error, success) => {
   if (error) {
     console.error("❌ Email configuration error:", error);
   } else {
